af5a5863c4457f2ccc0e45d9347eb68f667ddb91
YakuBrangJa <dbrangja@gmail.com>
2022-06-14 16:22:45 +0630

    repo to github

--- src/store/formCardContent.js
+++ src/store/formCardContent.js
@@ -67,6 +67,21 @@ const initialState = {
       },
       {
         index: 5,
+        name: "Occupation",
+        legend: {
+          en: "What is your occupation?",
+          mm: null,
+        },
+        options: [
+          "Government Staff",
+          "Company staff",
+          "Business Owner",
+          "Dependent",
+          "Others",
+        ],
+      },
+      {
+        index: 6,
         name: "Income (MMK)",
         legend: {
           en: "What is your monthly income (MMK)?",
@@ -81,50 +96,21 @@ const initialState = {
           "1,500,001 and above",
         ],
       },
-      {
-        index: 6,
-        name: "Department",
-        legend: {
-          en: "Which department are you working in?",
-          mm: "လက်ရှိအလုပ်လုပ်နေသည့်ဌာန",
-        },
-        options: [
-          "Programme",
-          "Finance",
-          "Human Resources",
-          "Admin and logistic",
-        ],
-      },
-      {
-        index: 7,
-        name: "Work Year",
-        legend: {
-          en: "How long you have been working in Metta?",
-          mm: "လုပ်သက်",
-        },
-        options: [
-          "Under 3 years",
-          "3 - 5 years",
-          "5 - 7 years",
-          "7 - 10 years",
-          "10 years and above",
-        ],
-      },
     ],
   },
 
   // SECTION B
   sectionB: {
-    title: "Section-B: Leadership Styles",
+    title: "Section-B: Respondent's opinion",
     instruction:
       "Please indicate how or the way that your supervisor/manager/leader engages in the described behaviors listed below and select to what extent do you agree or disagree by selecting the answer you chose",
-    group: "Leadership Styles",
+    group: "Respondent's opinion",
     content: [
       {
         index: 8,
         name: "index8",
         legend: {
-          en: "My supervisor retains the final decision authority",
+          en: "The food products can get anytime 24 hrs. a day at the online shopping.",
           mm: null,
         },
         options: opinions,
@@ -133,35 +119,79 @@ const initialState = {
         index: 9,
         name: "index9",
         legend: {
-          en: "My supervisor includes me in decision making but retains the final decision making authority",
+          en: "I save a lot of time by shopping food products on the Internet.",
           mm: null,
         },
         options: opinions,
       },
-    ],
-  },
-
-  // SECTION C
-  sectionC: {
-    title: "Section-C: Employee Performance",
-    instruction:
-      "You are requested to rate yourself against each statement to indicate yourself assesment of your own performance",
-    group: "Employee Performance",
-    content: [
       {
         index: 10,
         name: "index10",
         legend: {
-          en: "I clearly understand the vision, mission and objectives of the organisation",
+          en: "There are variety of food on the online shopping.",
           mm: null,
         },
         options: opinions,
       },
       {
         index: 11,
-        name: "Index11",
+        name: "index11",
         legend: {
-          en: "I am motivated to work",
+          en: "I can save money by comparing the prices offered at different shops for purchasing food.",
+          mm: null,
+        },
+        options: opinions,
+      },
+      {
+        index: 12,
+        name: "index12",
+        legend: {
+          en: "I worry to buy food online because of a lack of the ability to check physical objects and taste.",
+          mm: null,
+        },
+        options: opinions,
+      },
+      {
+        index: 13,
+        name: "index13",
+        legend: {
+          en: "I can only see the limited knowledge of the foods posted by online shops.",
+          mm: null,
+        },
+        options: opinions,
+      },
+      {
+        index: 14,
+        name: "index14",
+        legend: {
+          en: "I worry about being cheated and losing money through online shopping.",
+          mm: null,
+        },
+        options: opinions,
+      },
+      {
+        index: 15,
+        name: "index15",
+        legend: {
+          en: "I worry about the waste of time if products break or fail after a few days and need replacement.",
+          mm: null,
+        },
+        options: opinions,
+      },
+      {
+        index: 16,
+        name: "index16",
+        legend: {
+          en: "I worry about the close expiry date being delivered when I order food online.",
+          mm: null,
+        },
+        options: opinions,
+      },
+      {
+        index: 16,
+        name: "index16",
+        legend: {
+          en: "It is easy to purchase food products via the mobile application.",
           mm: null,
         },
         options: opinions,
